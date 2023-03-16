9a569d87949012ef1b21d50caca18c7183e6be74
Ha-neu-l <aafanehajar@gmail.com>
2023-03-16 16:26:14 -0700

    4puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - printing a line in puts file
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
